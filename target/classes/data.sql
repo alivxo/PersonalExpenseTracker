37b27a4fca5da1b86f91c58a032896e34fe76b79
Sumanth Samala <chintusamala96@gmail.com>
2024-12-05 10:52:38 +0000

    Remove target directory from tracking

--- target/classes/data.sql
+++
@@ -1,4 +0,0 @@
--- INSERTING DATA INTO THE USERS TABLE
-INSERT INTO Users (username, password, email) VALUES
-('sumanth', 'hashed_password_1', 'sumanth@example.com'),
-('john_doe', 'hashed_password_3', 'john.doe@example.com');
